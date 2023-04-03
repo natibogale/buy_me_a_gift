1b062269370651f371afb5645545f23749939164
natibogale45@gmail.com <natibogale45@gmail.com>
2023-04-03 06:18:40 +0300

    feat: finished login and signup funtionality

---
+++ authentication/views.py
@@ -0,0 +1,38 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import SignupSerializer, SigninSerializer
+
+
+class SignupView(CreateAPIView):
+    serializer_class = SignupSerializer
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class SigninView(CreateAPIView):
+    serializer_class = SigninSerializer
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data['user']
+        refresh = RefreshToken.for_user(user)
+
+        response_data = {
+            'access_token': str(refresh.access_token),
+            'refresh_token': str(refresh),
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
